1b078c011347e214485ba8eaa502b804c3d93023
LAI <LAI@d12fc58c-778e-2e4e-8f78-730f8c56e3ab>
2013-04-23 15:04:12 +0000

    Добавлена загрузка настроек модема
    git-svn-id: http://192.168.1.98/svn/BelAZ/Radio/ModemLab/Soft_Qt/VarModemStudy@1311 d12fc58c-778e-2e4e-8f78-730f8c56e3ab

--- connectioncontrol.cpp
+++ connectioncontrol.cpp
@@ -5,6 +5,7 @@
 #include "BitBusPipes/PipeMgr.h"
 #include <QMutex>
 #include <QList>
+#include "programsettings.h"
 
 //==============================================================================
 CConnectionDescriptor::CConnectionDescriptor()
@@ -195,16 +196,14 @@ void CConnectionControl::slotPortConnected(QString portName)
 }
 
 //==============================================================================
-// временно будем хранить настройки здесь.
-//char ucUIDtx[] = {0x6e,0xcf,0xb0,0xcc,0x3c,0xbd,0x4b,0x40,0x84,0xc3,0xe0,0x5b,0x4d,0xb0,0x82,0xb0};
-char ucUIDtx[] =   {0x36,0xf3,0x01,0xee,0x85,0xb4,0x4f,0x82,0xb6,0xe6,0x7c,0xba,0xc8,0xe4,0xc6,0xa8};
-
-const QByteArray txUID(ucUIDtx, sizeof (ucUIDtx));
-
-
 void CConnectionControl::slotPortDisconnected(QString portName)
 {
     int iInd = CConnectionDescriptor::FindIndexByPortName(m_connectionsList, portName);
+
+    QByteArray txUID;
+    ProgramSettings ps;
+    txUID = ps.value("TxUID").toByteArray();
+
     if( txUID == m_connectionsList[iInd]->m_DeviceUID )
     {
         m_TxPort = NULL;
@@ -219,6 +218,10 @@ void CConnectionControl::slotNewDeviceFound(CConnectionDescriptor *connDescr)
 {
     QString strData(connDescr->m_DeviceUID.toHex());
     qDebug()<< "New device connected, UID =" << strData ;
+
+    QByteArray txUID;
+    ProgramSettings ps;
+    txUID = ps.value("TxUID").toByteArray();
 
     if( txUID == connDescr->m_DeviceUID )
     {
